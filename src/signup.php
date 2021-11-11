7e56342eca7e1c16214148c2416acea7ad73cab7
kstewart1325 <56324820+kstewart1325@users.noreply.github.com>
2021-11-11 10:31:53 -0500

    fixed login and signup pages

--- src/signup.php
+++ src/signup.php
@@ -6,17 +6,11 @@ $url = "http://localhost:8070/";
 include 'db_connection.php';
 $conn = OpenCon();
 
-if ($conn->connect_error) {
-    die("Connection failed: " 
-        . $conn->connect_error);
-} else {
-    echo "Connection established<br>";
-}
-
 $resubmit = false;
+$error_message = "";
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    //check if values are empty, ask them to resubmit if not
+    //stores data from form
     $firstname = $_POST['first_name'];
     $lastname = $_POST['last_name'];
     $username = $_POST['username'];
@@ -24,62 +18,64 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $birthday = $_POST['birthday'];
     $password = $_POST['password'];
 
+    //checks if values are empty, ask them to resubmit if not
     if(empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($birthday) || empty($password)){
-        echo "Some fields left blank. Please fill out entire form.<br>";
+        $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
         $resubmit = true;
     }
 
-    //checks if username and/or email is in use
-    $sql = "SELECT username FROM User WHERE username LIKE " . $username . " LIMIT 0 , 30";
+    //queries username and email
+    $sql = "SELECT username FROM Account WHERE username=\"$username\" LIMIT 0 , 30";
     $result = $conn->query($sql);
 
-    $sql = "SELECT email FROM User WHERE email LIKE " . $email;
-    $result2 = $conn->query($sql);
-    
-    if($result === true || $result2 === true) {
-        $echo("Username or email already in use. Please choose another.<br>");
+    $sql2 = "SELECT email FROM Account WHERE email=\"$email\" LIMIT 0 , 30";
+    $result2 = $conn->query($sql2);
+
+    //if username and/or email already in use
+    if($result->num_rows > 0 && $result2->num_rows > 0) {
+        $error_message = "<br>Username and email already in use. Please choose another.<br>";
+        $resubmit = true;
+    } else if($result->num_rows > 0){
+        $error_message = "<br>Username already in use. Please choose another.<br>";
+        $resubmit = true;
+    } else if($result2->num_rows > 0){
+        $error_message = "<br>Email already in use. Please choose another.<br>";
         $resubmit = true;
     }
 
-    // // adds info to "user" database if username and email available
-    // if($resubmit === false){
-    //     //calculates appropriate userid
-    //     $id = 0;
-    //     $sql = "SELECT MAX(userid) AS id FROM user";
-    //     $result = $conn->query($sql);
-    //     if($result->num_rows > 0) {
-    //         $row = $result->fetch_assoc();
-    //         $id = $row["id"] + 1;
-    //     }
+    // adds info to 'Account' database if username and email available
+    if($resubmit === false){
+        //calculates appropriate user_id
+        $id = 0;
+        $sql = "SELECT MAX(user_id) AS id FROM Account";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $id = $row["id"] + 1;
+            echo "New ID: $id";
+        }
 
-    //     $sql = "INSERT INTO user VALUES 
-    //     ('" . $id . "', '" . $firstname . "', '" . $lastname . "', '" . $username . 
-    //     "', '" . $email . "', 'CURRENT_TIMESTAMP', '" . $birthday . "', '" . $password . "')";
-    //     $result = $conn->query($sql);
+        $sql = "INSERT INTO Account VALUES 
+        ('$id', '$firstname', '$lastname', '$username', '$email', CURRENT_TIMESTAMP, '$birthday', '$password')";
+        $result = $conn->query($sql);
 
-    //     if ($result === TRUE) {
-    //         $echo("record inserted successfully<br>");
-    //     } else {
-    //         $echo("Error: " . $sql . "<br>" . $conn->error);
-    //     }
-    // }
+        if ($result === TRUE) {
+            header('Location: '. $url . $path . 'login.html');
+        } else {
+            $echo("Error: " . $sql . "<br>" . $conn->error);
+        }
+    }
 
-    // $errorMessage = <<< EOPAGE
-    // <p></p>
-    // EOPAGE;
-
-    // echo $pageContents;
 } else {
     echo "POST not submitted<br>";
 }
 
 CloseCon($conn);
 
-//redirects to signup page if form must be resubmitted
-//else redirects to home page
-// if($resubmit === true){
-//     header('Location: ' . $url . $path . 'signup.html');
-// } else {
-//     header('Location: ' . $url . $path . 'index.html');
-// }
+//displays signup page with appropriate error message
+if($resubmit === true){
+    include 'signup.html';
+    echo $error_message;
+}
+
  ?>
