1085a08d75283edf816adbf20b308a9f1ff3f637
kstewart1325 <56324820+kstewart1325@users.noreply.github.com>
2021-11-15 14:26:47 -0500

    Added channel placeholder

--- src/channel.php
+++ src/channel.php
@@ -3,7 +3,19 @@
 function getChannelPage(){
     $html = <<< PAGE
     <div class="home">
-       <p>Channel Page</p>
+       <div class="row">
+            <h3>My Uploads</h3>
+            <img class="media" src="../media/image-placeholder.png" alt="Image Placeholder">
+            <img class="media" src="../media/image-placeholder.png" alt="Image Placeholder">
+            <img class="media" src="../media/video-placeholder.png" alt="Video Placeholder">
+       </div>
+       <div class="row">
+            <h3>Favorites </h3>
+            <img class="media" src="../media/image-placeholder.png" alt="Image Placeholder">
+            <img class="media" src="../media/video-placeholder.png" alt="Video Placeholder">
+            <img class="media" src="../media/video-placeholder.png" alt="Video Placeholder">
+            <img class="media" src="../media/image-placeholder.png" alt="Image Placeholder">
+       </div>
     </div>
     PAGE;
 
