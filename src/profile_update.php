ab543c7f0e2ffc856c1aeff5f2bd8c61289cefc7
BringTheParty <bartjohnson1999@gmail.com>
2021-11-16 08:56:23 -0500

    Last profile update

--- src/profile_update.php
+++ src/profile_update.php
@@ -14,7 +14,10 @@ include 'db_connection.php';
 $conn = OpenCon();
 
 $resubmit = false;
+$submitted = false;
 $error_message = "";
+
+$session_user = $_SESSION['user_id'];
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
     //stores data from form
@@ -23,53 +26,6 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-    //checks if values are empty, ask them to resubmit if not
-    //if(empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($birthday) || empty($password)){
-    //    $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
-    //    $resubmit = true;
-    //}
-
-    if(!empty($firstname)){
-        $sql = "UPDATE Account
-        SET 
-            first_name = \"$firstname\"
-        WHERE 
-            user_id = 3";
-        $conn->query($sql);
-    }
-    if(!empty($lastname)){
-        $sql = "UPDATE Account
-        SET 
-            last_name = \"$lastname\"
-        WHERE 
-            user_id = 3";
-        $conn->query($sql);
-    }
-    if(!empty($username)){
-        $sql = "UPDATE Account
-        SET 
-            username = \"$username\"
-        WHERE 
-            user_id = 3";
-        $conn->query($sql);
-    }
-    if(!empty($email)){
-        $sql = "UPDATE Account
-        SET 
-            email = \"$email\"
-        WHERE 
-            user_id = 3";
-        $conn->query($sql);
-    }
-    if(!empty($password)){
-        $sql = "UPDATE Account
-        SET 
-            password = \"$password\"
-        WHERE 
-            user_id = 3";
-        $conn->query($sql);
-    }
 
     //queries username and email
     $sql = "SELECT username FROM Account WHERE username=\"$username\" LIMIT 0 , 30";
@@ -82,35 +38,123 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     if($result->num_rows > 0 && $result2->num_rows > 0) {
         $error_message = "<br>Username and email already in use. Please choose another.<br>";
         $resubmit = true;
-    } else if($result->num_rows > 0){
+    } 
+    else if($result->num_rows > 0){
         $error_message = "<br>Username already in use. Please choose another.<br>";
         $resubmit = true;
-    } else if($result2->num_rows > 0){
+    } 
+    else if($result2->num_rows > 0){
         $error_message = "<br>Email already in use. Please choose another.<br>";
         $resubmit = true;
-    }    
-
-    // adds info to 'Account' database if username and email available
-    if($resubmit === false){
-        //checks if email is valid
-        if(strpos($email, "@") === false || strpos($email, ".com") === false){
-            $error_message = "<br>Invalid Email. Please choose another.<br>";
-            $resubmit = true;
+    } 
+    else if(!empty($email) && (strpos($email, "@") === false || strpos($email, ".")) === false){
+        $error_message = "<br>Invalid Email. Please choose another.<br>";
+        $resubmit = true;
+    } 
+    else{
+        //update the information if the box is filled in
+        if(!empty($firstname)){
+            $sql = "UPDATE Account
+            SET 
+                first_name = \"$firstname\"
+            WHERE 
+                user_id = \"$session_user\"";
+            $conn->query($sql);
+            $submitted = true;
+        }
+        if(!empty($lastname)){
+            $sql = "UPDATE Account
+            SET 
+                last_name = \"$lastname\"
+            WHERE 
+                user_id = \"$session_user\"";
+            $conn->query($sql);
+            $submitted = true;
+        }
+        if(!empty($username)){
+            $sql = "UPDATE Account
+            SET 
+                username = \"$username\"
+            WHERE 
+                user_id = \"$session_user\"";
+            $conn->query($sql);
+            $submitted = true;
+        }
+        if(!empty($email)){
+            $sql = "UPDATE Account
+            SET 
+                email = \"$email\"
+            WHERE 
+                user_id = \"$session_user\"";
+            $conn->query($sql);
+            $submitted = true;
+        }
+        if(!empty($password)){
+            $sql = "UPDATE Account
+            SET 
+                password = \"$password\"
+            WHERE 
+                user_id = \"$session_user\"";
+            $conn->query($sql);
+            $submitted = true;
         }
     }
-
-
-
-} else {
-    echo "POST not submitted<br>";
-}
+} 
 
 CloseCon($conn);
 
+$html = <<< PAGE
+                                <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Profile Update</title>
+    </head>
+    <body>
+        <span id="profile_update">
+        <form action="profile_update.php" method="post">
+            <fieldset>
+            <legend>Update Profile Information</legend>
+            <p>
+                <label for="first_name">First Name: </label>
+                <input type="text" id="first_name" name="first_name" /><br />
+            </p>
+            <p>
+                <label for="last_name">Last Name: </label>
+                <input type="text" id="last_name" name="last_name" /><br />
+            </p>
+            <p>
+                <label for="username">Username: </label>
+                <input type="text" id="username" name="username" /><br />
+            </p>
+            <p>
+                <label for="email">Email: </label>
+                <input type="text " id="email" name="email" /><br />
+            </p>
+            <p>
+                <label for="password">Password: </label>
+                <input type="text" id="password" name="password" /><br />
+            </p>
+            <p>
+                <button type="submit" value="Send" action="profile_update.php">Save </button> 
+                <button type="reset">Cancel </button>
+            </p>
+    PAGE;
+
 //displays signup page with appropriate error message
 if($resubmit === true){
-    include 'profile_update.html';
-    echo $error_message;
+    $html .= $error_message;
 }
+if($submitted === true){
+    $html .= "Profile successfully updated";
+}
+
+$html .= <<< PAGE
+      </form>
+    </span>
+  </body>
+</html>
+PAGE;
+
+echo $html;
 
  ?>
