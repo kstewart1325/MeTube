6cbf9c2234cba6ed605eb3b023b030abe49ce0ca
kstewart1325 <56324820+kstewart1325@users.noreply.github.com>
2021-11-16 11:50:59 -0500

    uploaded media files displayed on user channel page

--- src/upload.php
+++ src/upload.php
@@ -4,6 +4,8 @@
 $path = "MeTube/src/";
 $url = "http://localhost:8070/";
 
+if(!session_id()) session_start();
+
 include 'db_connection.php';
 $conn = OpenCon();
 
@@ -11,48 +13,54 @@ $resubmit = false;
 $error_message = "";
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    //stores data from form
-    $file = $_FILES['mediafile'];
-    $filesize = $_FILES['mediafile']['size'];
-    $filetype = $_FILES['mediafile']['type'];
+  //stores data from form
+  $file = $_FILES['mediafile'];
+  $filesize = $_FILES['mediafile']['size'];
+  $filetype = $_FILES['mediafile']['type'];
 
-    $title = $_POST['title'];
-    $category = $_POST['category'];
-    $desc = $_POST['description'];
-    $keywords = $_POST['keywords'];
+  $title = $_POST['title'];
+  $category = $_POST['category'];
+  $desc = $_POST['description'];
+  $keywords = $_POST['keywords'];
 
-    //checks if values are empty, ask them to resubmit if not
-    if(empty($file) || empty($title) || empty($category) || empty($desc) || empty($keywords)){
-      $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
+  //checks if values are empty, ask them to resubmit if not
+  if(empty($file) || empty($title) || $category==="blank" || empty($desc) || empty($keywords)){
+    $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
+    $resubmit = true;
+  }
+
+  // adds info to 'Mediafiles' database
+  if($resubmit === false){
+    $user_id = $_SESSION['user_id'];
+
+    //checks if filename already exists
+    $sql = "SELECT `media_title` FROM Mediafiles WHERE `media_title`=\"$title\" LIMIT 0 , 30";
+    $result = $conn->query($sql);
+
+    if($result->num_rows === 0) {
+      //calculates appropriate media_id
+      $id = 0;
+      $sql = "SELECT MAX(media_id) AS id FROM Mediafiles";
+      $result = $conn->query($sql);
+      if($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+          $id = $row["id"] + 1;
+      }
+
+      $sql = "INSERT INTO Mediafiles VALUES 
+      ('$id', '$user_id', '$title', '$filetype', '$filesize', '$category', CURRENT_TIMESTAMP, '0', '$desc', '$file')";
+      $result = $conn->query($sql);
+
+      if ($result === TRUE) {
+          header('Location: '. $url . $path . 'index.php?page=channel');
+      } else {
+          $echo("Error: " . $sql . "<br>" . $conn->error);
+      }
+    } else {
+      $error_message = "<br><br>Media title already exists. Please choose another.<br>";
       $resubmit = true;
     }
-
-    //check if existing title exists?
-
-    // adds info to 'Mediafiles' database
-    if($resubmit === false){
-        $user_id = $_SESSION['user_id'];
-        
-        //calculates appropriate media_id
-        $id = 0;
-        $sql = "SELECT MAX(media_id) AS id FROM Mediafiles";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $id = $row["id"] + 1;
-        }
-
-        $sql = "INSERT INTO Mediafiles VALUES 
-        ('$id', '$user_id', '$title', '$filetype', '$filesize', '$category', CURRENT_TIMESTAMP, '0', '$desc', '$file')";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            header('Location: '. $url . $path . 'index.php?page=channel');
-        } else {
-            $echo("Error: " . $sql . "<br>" . $conn->error);
-        }
-    }
-
+  }
 }
 
 $html = <<< PAGE
