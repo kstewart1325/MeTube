817817d6c778c4692bac83bac4bb379707696510
kstewart1325 <56324820+kstewart1325@users.noreply.github.com>
2021-11-16 10:20:13 -0500

    finish html form and db insert

--- src/upload.php
+++ src/upload.php
@@ -12,62 +12,42 @@ $error_message = "";
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
     //stores data from form
-    $firstname = $_POST['first_name'];
-    $lastname = $_POST['last_name'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $birthday = $_POST['birthday'];
-    $password = $_POST['password'];
+    $file = $_FILES['mediafile'];
+    $filesize = $_FILES['mediafile']['size'];
+    $filetype = $_FILES['mediafile']['type'];
+
+    $title = $_POST['title'];
+    $category = $_POST['category'];
+    $desc = $_POST['description'];
+    $keywords = $_POST['keywords'];
 
     //checks if values are empty, ask them to resubmit if not
-    if(empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($birthday) || empty($password)){
-        $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
-        $resubmit = true;
+    if(empty($file) || empty($title) || empty($category) || empty($desc) || empty($keywords)){
+      $error_message = "<br>Some fields left blank. Please fill out entire form.<br>";
+      $resubmit = true;
     }
 
-    //queries username and email
-    $sql = "SELECT username FROM Account WHERE username=\"$username\" LIMIT 0 , 30";
-    $result = $conn->query($sql);
+    //check if existing title exists?
 
-    $sql2 = "SELECT email FROM Account WHERE email=\"$email\" LIMIT 0 , 30";
-    $result2 = $conn->query($sql2);
-
-    //checks if username and/or email already in use
-    if($result->num_rows > 0 && $result2->num_rows > 0) {
-        $error_message = "<br>Username and email already in use. Please choose another.<br>";
-        $resubmit = true;
-    } else if($result->num_rows > 0){
-        $error_message = "<br>Username already in use. Please choose another.<br>";
-        $resubmit = true;
-    } else if($result2->num_rows > 0){
-        $error_message = "<br>Email already in use. Please choose another.<br>";
-        $resubmit = true;
-    }
-
-    // adds info to 'Account' database if username and email available
+    // adds info to 'Mediafiles' database
     if($resubmit === false){
-        //checks if email is valid
-        if(strpos($email, "@") === false || strpos($email, ".") === false){
-            $error_message = "<br>Invalid Email. Please choose another.<br>";
-            $resubmit = true;
-        }
-
-        //calculates appropriate user_id
+        $user_id = $_SESSION['user_id'];
+        
+        //calculates appropriate media_id
         $id = 0;
-        $sql = "SELECT MAX(user_id) AS id FROM Account";
+        $sql = "SELECT MAX(media_id) AS id FROM Mediafiles";
         $result = $conn->query($sql);
         if($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $id = $row["id"] + 1;
-            echo "New ID: $id";
         }
 
-        $sql = "INSERT INTO Account VALUES 
-        ('$id', '$firstname', '$lastname', '$username', '$email', CURRENT_TIMESTAMP, '$birthday', '$password')";
+        $sql = "INSERT INTO Mediafiles VALUES 
+        ('$id', '$user_id', '$title', '$filetype', '$filesize', '$category', CURRENT_TIMESTAMP, '0', '$desc', '$file')";
         $result = $conn->query($sql);
 
         if ($result === TRUE) {
-            header('Location: '. $url . $path . 'login.php');
+            header('Location: '. $url . $path . 'index.php?page=channel');
         } else {
             $echo("Error: " . $sql . "<br>" . $conn->error);
         }
