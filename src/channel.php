7e1eab068bb60cf8ae10060a183639c2832053d2
emeliaf <56324807+emeliaf@users.noreply.github.com>
2021-11-21 19:51:28 -0500

    Revert "changes didn't go through on last merge. fixed session variables, add…"

--- src/channel.php
+++ src/channel.php
@@ -1,7 +1,7 @@
 <?php 
 
 function getChannelPage(){
-     include_once 'db_connection.php';
+     include 'db_connection.php';
 
      $conn = openCon();
 
@@ -16,7 +16,6 @@ function getChannelPage(){
 
      $html = "<div class=\"welcome\" style=\"padding-left: 20px\">";
      $html .= "<h2>Hello $name!</h2>";
-     $html.= "<br><a href=\"/MeTube/src/index.php?page=favorites\">Favorites</a> <a href=\"/MeTube/src/index.php?page=playlists\">Playlists</a> <a href=\"/MeTube/src/index.php?page=contacts\">Contacts</a>";
      $html .= "</div>";
 
      $html .= <<< PAGE
