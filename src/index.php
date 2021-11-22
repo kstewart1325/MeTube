57fa75493083c0f8dda0b2d21360009888115fe6
emeliaf <emeliaf@clemson.edu>
2021-11-21 19:27:23 -0500

    updated index w/ stuff for playlists/favs/cont

--- src/index.php
+++ src/index.php
@@ -5,6 +5,10 @@ include 'home.php';
 include 'channel.php';
 include 'media.php';
 include 'search.php';
+include 'playlists.php';
+include 'contacts.php';
+include 'favorites.php';
+
 
 $path = "MeTube/src/";
 $url = "http://localhost:8070/";
@@ -85,6 +89,15 @@ if($currentPage === "home"){
     header('Location: '. $url . $path . 'index.php');
 }else if($currentPage === "search"){
     $html .= $search_html;
+}else if($currentPage === "playlists"){
+    $html .= "<i>$currentMsg</i><br>";
+    $html .= $play_html;
+}else if($currentPage === "contacts"){
+    $html .= "<i>$currentMsg</i><br>";
+    $html .= $contacts_html;
+}else if($currentPage === "favorites"){
+    $html .= "<i>$currentMsg</i><br>";
+    $html .= $fav_html;
 }
 
 $html .= <<< PAGE
